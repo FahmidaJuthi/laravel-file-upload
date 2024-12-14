bbc5981c3ba0f2f271dd9f80be681b7e51e162f4
FahmidaJuthi <jtitly@gmail.com>
2024-12-14 16:45:52 +0600

    file

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Cookie;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+       
+        $name = "Donald Trump";
+        $age = "75";
+
+       
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age
+        ];
+
+        $cookie = cookie(
+            'access_token',        
+            '123-XYZ',             
+            1,                     
+            '/',                   
+            $_SERVER['SERVER_NAME'], 
+            false,                 
+            true                   
+        );
+
+        
+        return response($data, 200)->cookie($cookie);
+    }
+}
